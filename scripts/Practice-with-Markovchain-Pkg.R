7f733af3fe12a16df0072e625a386777e0b56b81
Steph Reynolds <sreynolds1027@gmail.com>
2022-04-05 16:52:39 -0700

    Add comments to script

--- scripts/Practice-with-Markovchain-Pkg.R
+++ scripts/Practice-with-Markovchain-Pkg.R
@@ -2,6 +2,8 @@
 
 library(markovchain)
 
+# Set matrix
+    # All rows or all columns sum to 1 
 mat <- matrix(c(.25, .25, .25, .25,
                 .25, .25, .25, .25,
                 0, 0, 1, 0,
@@ -10,30 +12,56 @@ mat <- matrix(c(.25, .25, .25, .25,
               nrow=4, 
               ncol=4)
 
+# Create Markov chain  
 mc <- as(mat, "markovchain")
 
+# Plot Markov chain
 plot(mc, main="Covid Stages Markov Chain")
 
+# Define probabilities of initial state 
 init_state <- c(.5, .5, 0, 0)
 
+# Calculate likelihood of each state after 2 days 
 after2Days <- init_state * (mc^2)
 after2Days
 
 mc^2
 
+# Print states of Markov chain
 states(mc)
-dim(mc)
-t(mc)
-transitionProbability(mc)
 
+# Number of states of dimensions
+dim(mc)
+
+# Print transition matrix
+t(mc)
+
+# Obtain transition probabilities
+    # Must provide initial state (t0) and subsequent state (t1)
+transitionProbability(mc, t0 = "s1", t1 = "s2")
+
+# Convert Markov chain to a data frame
 as(mc, "data.frame")
 
+# Shows the closed, recurrent, and transient classes. 
+# Show absorbing states 
 summary(mc)
 
+# Vector of steady states in matrix form
 steadyStates(mc)
+
+# Transient states
 transientStates(mc)
+
+# Absorbing states
 absorbingStates(mc)
 
+
+################################################################################
+#### igraph package ############################################################
+################################################################################
+
+# Load package
 library(igraph)
 
 mc.igraph <- as(mc, "igraph")
@@ -42,18 +70,18 @@ mc.igraph <- as(mc, "igraph")
 SCC <- clusters(mc.igraph, mode="strong")
 V(mc.igraph)$color <- rainbow(SCC$no)[SCC$membership]
 
-#plotting
+# Shows communicating classes or strongly connected components 
 plot(mc.igraph, mark.groups = split(1:vcount(mc.igraph), SCC$membership),
      main="Communicating classes - strongly connected components")
 
-#load df
+# Preview first 20 rows of stagecat variable
 df$stagecat[1:20]
 
-#before next step, need to split data into lists by ID 
+# Before next step, need to split data into lists by ID 
 df <- split(df, f = df$ID)
 df 
 
-#obtaining the empirical transition matrix 
+# Obtain empirical transition matrix 
 createSequenceMatrix(stringchar = df$stagecat)  # this doesnt work on lists 
 #lapply(df, createSequenceMatrix)
 #try for loop
